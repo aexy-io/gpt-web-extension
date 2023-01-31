dc1e6168021bda7aee84bf62f251ace3309ad464
Bhanu <bhanu423@gmail.com>
2023-01-31 12:39:25 +0530

    updated extension

--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Hello } from "@src/components/hello";
 import browser, { Tabs } from "webextension-polyfill";
 import { Scroller } from "@src/components/scroller";
@@ -47,18 +47,97 @@ function executeScript(position: number): void {
         });
 }
 
-// // // //
+const popularTab = () => {
+    return (
+        <div>
+            {" "}
+            <div className="mb-6">
+                <ul>
+                    <li className="mb-4">
+                        <a
+                            className="flex items-center p-4 text-white bg-blue-500 rounded-xl"
+                            href="#"
+                        >
+                            <span className="ml-4 text-sm font-semibold">
+                                Viral Post Generator
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="mb-4">
+                        <a
+                            className="flex items-center p-4 text-white bg-blue-500 rounded-xl"
+                            href="#"
+                        >
+                            <span className="ml-4 text-sm font-semibold">
+                                Jira Generator
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const baseURL = "http://localhost:5678";
 
 export function Popup() {
     // Sends the `popupMounted` event
     React.useEffect(() => {
         browser.runtime.sendMessage({ popupMounted: true });
     }, []);
-
+    const [tabState, setTabState] = useState(0);
     // Renders the component tree
     return (
         <div className={css.popupContainer}>
-            <div className="mx-4 my-4">
+            <div className="container h-full">
+                <div className="bg-white mb-2">
+                    <nav className="flex flex-row pl-2">
+                        <button
+                            className={` py-4 px-6 block hover:text-blue-500 focus:outline-none ${
+                                tabState === 0
+                                    ? "text-blue-500 border-b-2 font-medium border-blue-500"
+                                    : "text-gray-600"
+                            }`}
+                            onClick={() => {
+                                setTabState(0);
+                            }}
+                        >
+                            Recommended
+                        </button>
+                        <button
+                            className={` py-4 px-6 block hover:text-blue-500 focus:outline-none ${
+                                tabState === 1
+                                    ? "text-blue-500 border-b-2 font-medium border-blue-500"
+                                    : "text-gray-600"
+                            }`}
+                            onClick={() => {
+                                setTabState(1);
+                            }}
+                        >
+                            Popular
+                        </button>
+                        <button
+                            className={` py-4 px-6 block hover:text-blue-500 focus:outline-none ${
+                                tabState === 2
+                                    ? "text-blue-500 border-b-2 font-medium border-blue-500"
+                                    : "text-gray-600"
+                            }`}
+                            onClick={() => {
+                                setTabState(2);
+                            }}
+                        >
+                            Saved
+                        </button>
+                    </nav>
+                </div>
+                {tabState === 0 && popularTab()}
+                {tabState === 1 && <span>tab 1</span>}
+                {tabState === 2 && <span>tab 2</span>}
+                {tabState === 3 && <span>tab 3</span>}
+            </div>
+            {/* <div className="mx-4 my-4">
                 <Hello />
                 <hr />
                 <Scroller
@@ -69,7 +148,7 @@ export function Popup() {
                         executeScript(scrollToBottomPosition);
                     }}
                 />
-            </div>
+            </div> */}
         </div>
     );
 }
